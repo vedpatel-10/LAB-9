3ad27b4d2e266f5c0ed9e38805dd88448fa5de4b
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 15:19:29 +0530

    Update Q-1.4.py

--- Q-1.4.py
+++ Q-1.4.py
@@ -11,3 +11,11 @@ sub3 = int(input("subject 3: "))
 sub4 = int(input("subject 4: "))
 sub5 = int(input("subject 5: "))
 print("Total and Average values are",sum_avg(sub1,sub2,sub3,sub4,sub5),"respectively")
+
+#OUTPUT:
+# subject 1: 77
+# subject 2: 81
+# subject 3: 90
+# subject 4: 88
+# subject 5: 59
+# Total and Average values are (395, 79.0) respectively
