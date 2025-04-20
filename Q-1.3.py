30cbe15a821b3ed02e87c3a6528ad4568d147cbc
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 15:18:46 +0530

    Update Q-1.3.py

--- Q-1.3.py
+++ Q-1.3.py
@@ -1,1 +1,26 @@
-# array problem
+def create_array(x, y, z, value):
+    array = [[[value for _ in range(z)] for _ in range(y)] for _ in range(x)]
+    return array
+
+array_3d = create_array(3, 4, 5, 7)
+
+for i in range(len(array_3d)):
+    for j in range(len(array_3d[i])):
+        print(array_3d[i][j])
+    print()  
+
+#OUTPUT:
+# [7, 7, 7, 7, 7]
+# [7, 7, 7, 7, 7]
+# [7, 7, 7, 7, 7]
+# [7, 7, 7, 7, 7]     
+
+# [7, 7, 7, 7, 7]
+# [7, 7, 7, 7, 7]
+# [7, 7, 7, 7, 7]
+# [7, 7, 7, 7, 7]
+
+# [7, 7, 7, 7, 7]
+# [7, 7, 7, 7, 7]
+# [7, 7, 7, 7, 7]
+# [7, 7, 7, 7, 7]
